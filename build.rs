bae83334799402feabd7c22cc258713b7bc5ff7d
Euan Rochester <euan@rochester.me.uk>
2016-08-08 20:21:53 +0100

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,4 +5,5 @@ use std::env;
 fn main() {
   let vk_path = env::var("VK_SDK_PATH").unwrap();
   println!("cargo:rustc-link-search={}/Bin",vk_path)
+  println!("cargo:rustc-link-lib=static=vulkan-1");
 }
